8b5e51c5c7d4ca8ce861a29fcc45c38778d4c8a5
DESKTOP-2OI4MOA\adrij <adrian.joya.rios.vega@gmail.com>
2026-07-30 13:16:10 +0200

    feat. mejoras visuales y variables de colores

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,22 +1,31 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 import { MenuController } from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss'],
-  imports: [CommonModule],
   standalone: true,
+  imports: [CommonModule],
 })
 export class HeaderComponent {
   isProfileOpen = false;
 
-  constructor(private menuCtrl: MenuController) {}
+  constructor(
+    private menuCtrl: MenuController,
+    private router: Router,
+  ) {}
 
   async openMainMenu(): Promise<void> {
     await this.menuCtrl.enable(true, 'main-menu');
     await this.menuCtrl.open('main-menu');
+  }
+
+  goHome(): void {
+    this.closeProfileMenu();
+    this.router.navigate(['/home']);
   }
 
   toggleProfileMenu(): void {
