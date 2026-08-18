43d72c1784b692afe36eda81fdf2fc5d5e153ebe
DESKTOP-2OI4MOA\adrij <adrian.joya.rios.vega@gmail.com>
2026-08-18 15:47:07 +0200

    capacitor v1

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'es.joinmatch.app',
+  appName: 'JoinMatch',
+  webDir: 'dist/football-app/browser',
+};
+
+export default config;
